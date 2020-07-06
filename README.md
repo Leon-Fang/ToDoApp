0dc8ee02089c159dd160de2a4cc4dd56a6e802c0
LeonFang <leon.fang@sap.com>
2020-07-06 17:50:25 +0800

    implement login features

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ToDoApp
+super user: 
+    - name: admin
+    - password: P2ssword01
+staff user:
+    - name: lfang
+    - password: long82...
--- signin/views.py
+++ signin/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, HttpResponse,redirect
+from django.contrib import auth
+from tasks import *
 
 # Create your views here.
+def index(request):       
+    return render(request,'index.html')
+
+def login_view(request):
+    username = request.POST.get('username','')
+    password = request.POST.get('password','')
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        auth.login(request, user)
+    #    return HttpResponse("welcome! " + username)
+        return redirect('list')
+    else:
+        return render(request,'index.html')
+
+def logout_view(request):
+    auth.logout(request)
+    return HttpResponseRedirect("index.html")
